b3545f27ab1508ca6e6e0281ae775aebfd0724cd
Sh6abrA <andpro22223@gmail.com>
2021-03-28 13:40:02 +0500

    Исправил баг в работе ГСЧ, добавил пробелы в коде

--- GPSCH.cs
+++ GPSCH.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace GSCH
 {
     class GPSCH
     {
-
-
         public int const1 = 5;
         public int const2 = 1;
         public int x;
         public GPSCH(int bit, int y)
         {
             Random random = new Random();
-
-          
-
+            if (y == 0)
+            {
+                y = random.Next(0, bit);
+            }
             x = y;
             x = (x * const1 + const2) % bit;
         }
-
     }
 }
